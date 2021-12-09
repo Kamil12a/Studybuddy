fff2e5e494fe6f061998ffabd09c95b5f725b1d4
KSchlagowski <kamilschlagowski@gmail.com>
2021-12-09 17:40:09 +0100

    #03 Create Initial migrations

---
+++ Backend/StudyBuddy.Infrastructure/Migrations/20211209163203_2ChangeStudenIdsToNotVirtual.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudyBuddy.Infrastructure.Migrations
+{
+    public partial class _2ChangeStudenIdsToNotVirtual : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
