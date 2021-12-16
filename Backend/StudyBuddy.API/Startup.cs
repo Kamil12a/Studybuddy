ce194570d49b96cc071a878b10c20725755d5fba
KSchlagowski <kamilschlagowski@gmail.com>
2021-12-16 13:09:14 +0100

    #05 Configure AutoMapper

--- Backend/StudyBuddy.API/Startup.cs
+++ Backend/StudyBuddy.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StudyBuddy.Application;
 using StudyBuddy.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -28,6 +29,7 @@ namespace StudyBuddy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
             services.AddDbContext<Context>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
---
+++ Backend/StudyBuddy.Application/Common/Mapping/IMapFrom.cs
@@ -0,0 +1,9 @@
+using AutoMapper;
+
+namespace StudyBuddy.Application.Common.Mapping
+{
+    public interface IMapFrom<T>
+    {
+         void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
