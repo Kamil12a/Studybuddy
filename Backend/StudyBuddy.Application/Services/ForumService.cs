188e4b6aed864fa237451f9089927ef370473459
KSchlagowski <kamilschlagowski@gmail.com>
2021-12-30 19:42:10 +0100

    #21 Create services for Group, Forum and User

---
+++ Backend/StudyBuddy.Application/Services/ForumService.cs
@@ -0,0 +1,28 @@
+using StudyBuddy.Application.Interfaces;
+using StudyBuddy.Application.ViewModels;
+
+namespace StudyBuddy.Application.Services
+{
+    public class ForumService : IForumService
+    {
+        public int AddPost(PostVm post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteGroupProperty(int groupPropertyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PostVm GetPost(int postId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdatePost(PostVm post)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
