81205cddee6b60c90bf7744b7dfc07efde5adcba
KSchlagowski <kamilschlagowski@gmail.com>
2022-01-08 17:50:40 +0100

    #21 Make new functionalities

--- Backend/StudyBuddy.Application/Common/Mapping/IMapFrom.cs
+++ Backend/StudyBuddy.Application/Common/Mapping/IMapFrom.cs
@@ -4,6 +4,6 @@ namespace StudyBuddy.Application.Common.Mapping
 {
     public interface IMapFrom<T>
     {
-         void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
     }
 }
--- Backend/StudyBuddy.Application/ViewModels/GroupVm.cs
+++ Backend/StudyBuddy.Application/ViewModels/GroupVm.cs
@@ -1,19 +1,21 @@
 using System.Collections.Generic;
+using AutoMapper;
 using StudyBuddy.Application.Common.Mapping;
 using StudyBuddy.Domain.Models;
 
 namespace StudyBuddy.Application.ViewModels
 {
-    public class GroupVm : IMapFrom<Group>
+    public class GroupVm : IMapFrom<StudyBuddy.Domain.Models.Group>
     {
         public int Id { get; set; }
         public int TutorId { get; set; }
-        public ICollection<User> StudentIds { get; set; }
+        //public ICollection<User> StudentIds { get; set; }
         public int AdminId { get; set; }
 
-        public void Mapping(MappingProfile profile)
+        public void Mapping(Profile profile)
         {
-            profile.CreateMap<Group, GroupVm>();
+            profile.CreateMap<GroupVm, StudyBuddy.Domain.Models.Group>()
+                .ReverseMap();
         }
     }
 }
