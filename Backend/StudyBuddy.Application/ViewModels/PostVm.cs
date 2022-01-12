13e81ed47f3ffbf5b0f14ff2036446817e291c39
KSchlagowski <kamilschlagowski@gmail.com>
2022-01-12 18:41:54 +0100

    #23 Add properties to Post model and view model

--- Backend/StudyBuddy.Application/ViewModels/PostVm.cs
+++ Backend/StudyBuddy.Application/ViewModels/PostVm.cs
@@ -1,7 +1,20 @@
+using AutoMapper;
+using StudyBuddy.Application.Common.Mapping;
+using StudyBuddy.Domain.Models;
+
 namespace StudyBuddy.Application.ViewModels
 {
-    public class PostVm
+    public class PostVm : IMapFrom<Post>
     {
-        
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public int OwnerId { get; set; }
+
+        public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Post, PostVm>();
+        }
     }
 }
