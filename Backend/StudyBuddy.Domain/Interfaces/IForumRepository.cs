bb255cced876c067a54761d17a59091064dd0182
KSchlagowski <kamilschlagowski@gmail.com>
2022-01-12 18:42:15 +0100

    #23 Add ForumRepo methods

--- Backend/StudyBuddy.Domain/Interfaces/IForumRepository.cs
+++ Backend/StudyBuddy.Domain/Interfaces/IForumRepository.cs
@@ -1,7 +1,12 @@
+using StudyBuddy.Domain.Models;
+
 namespace StudyBuddy.Domain.Interfaces
 {
     public interface IForumRepository
     {
-         
+        Post GetPost(int postId);
+        int AddPost(Post post);
+        void UpdatePost(Post post);
+        void DeletePost(int postId);
     }
 }
