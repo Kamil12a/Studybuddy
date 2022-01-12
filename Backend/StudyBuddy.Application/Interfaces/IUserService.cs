f09265c5760b9ead32c1a97115d4c72a1a910ae0
KSchlagowski <kamilschlagowski@gmail.com>
2022-01-12 10:50:41 +0100

    #23 Update IUserService for new methods

--- Backend/StudyBuddy.Application/Interfaces/IUserService.cs
+++ Backend/StudyBuddy.Application/Interfaces/IUserService.cs
@@ -1,7 +1,21 @@
+using System.Linq;
+using StudyBuddy.Application.ViewModels;
+
 namespace StudyBuddy.Application.Interfaces
 {
     public interface IUserService
     {
-         
+        UserVm GetUserById(int userId);
+        ListUserForListVm GetAllUsers();
+        ListUserForListVm GetAllUsers(int pageSize, int pageNo, string searchString);
+        UserPropertyVm GetUserPropertyVm(int userPropertyId);
+        int AddUser(UserVm userVm);
+        int AddUserPropertyVm(UserPropertyVm userPropertyVm);
+        void UpdateUser(UserVm userVm);
+        void UpdateUserProperty(UserPropertyVm userPropertyVm);
+        void DeleteUser(int userId);
+        void DeleteUserProperty(int userPropertyId); 
+        void DeleteUserAbsolute(int userId);
+        void DeleteUserPropertyAbsolute(int userPropertyId);
     }
 }
