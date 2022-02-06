b9f75ea5dd8665828e163621d0a78fb2daec4dc0
KSchlagowski <kamilschlagowski@gmail.com>
2022-02-06 18:40:23 +0100

    #26 Prepare models for showing all groups

---
+++ Backend/StudyBuddy.Application/ViewModels/GroupForListVm.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using StudyBuddy.Application.Common.Mapping;
+
+namespace StudyBuddy.Application.ViewModels
+{
+    public class GroupForListVm : IMapFrom<StudyBuddy.Domain.Models.Group>
+    {
+        public int Id { get; set; }
+        public int GroupOwnerId { get; set; }
+        public string ShortDescription { get; set; } = "ShortDescription - to be implemented";
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<StudyBuddy.Domain.Models.Group, GroupForListVm>()
+                .ReverseMap();
+        }
+    }
+}
