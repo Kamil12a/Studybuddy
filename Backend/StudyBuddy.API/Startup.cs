fd44e3f11dfb99e2e80906dcfa592d72e8443720
KSchlagowski <kamilschlagowski@gmail.com>
2022-02-08 19:08:41 +0100

    #14 Create logging

--- Backend/StudyBuddy.API/Startup.cs
+++ Backend/StudyBuddy.API/Startup.cs
@@ -68,8 +68,9 @@ namespace StudyBuddy.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("Logs/myLog-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
