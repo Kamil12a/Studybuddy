37ca5707c29684c6e86ed989eb4e03b01c43c6a1
KSchlagowski <kamilschlagowski@gmail.com>
2022-02-16 14:53:52 +0100

    #27 Edit PostVm

--- Backend/StudyBuddy.Application/ViewModels/PostVm.cs
+++ Backend/StudyBuddy.Application/ViewModels/PostVm.cs
@@ -7,14 +7,14 @@ namespace StudyBuddy.Application.ViewModels
     public class PostVm : IMapFrom<Post>
     {
         public int Id { get; set; }
-        public bool IsActive { get; set; }
         public int OwnerId { get; set; }
 
         public string Description { get; set; }
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<Post, PostVm>();
+            profile.CreateMap<Post, PostVm>()
+                .ReverseMap();
         }
     }
 }
