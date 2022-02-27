54b74ebb24a8bdd3f37254468755f6618ad0c3dd
KSchlagowski <kamilschlagowski@gmail.com>
2022-02-27 12:44:41 +0100

    #29 Extend Group model

--- Backend/StudyBuddy.Domain/Models/Group.cs
+++ Backend/StudyBuddy.Domain/Models/Group.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace StudyBuddy.Domain.Models
@@ -6,8 +7,13 @@ namespace StudyBuddy.Domain.Models
     {
         public int Id { get; set; }
         public bool IsActive { get; set; }
-        public int TutorId { get; set; }
+        public string TutorName { get => Tutor.Name; }
+        public string Place { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public DateTime MeetingDate { get; set; }
 
+        public virtual User Tutor { get; set; }
         public virtual User GroupOwner { get; set; }
         public virtual ICollection<User> JoinedUsers { get; set; }
         public virtual GroupProperty GroupProperty { get; set; }
