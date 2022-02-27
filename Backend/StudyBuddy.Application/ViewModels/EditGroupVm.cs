564e43a6a4ba98fab82e002fce912ec3b272fa5a
KSchlagowski <kamilschlagowski@gmail.com>
2022-02-27 14:41:35 +0100

    #29 Create EditGroupVm and prepare methods for it

---
+++ Backend/StudyBuddy.Application/ViewModels/EditGroupVm.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using StudyBuddy.Application.Common.Mapping;
+
+namespace StudyBuddy.Application.ViewModels
+{
+    public class EditGroupVm : IMapFrom<StudyBuddy.Domain.Models.Group>
+    {
+        public int Id { get; set; }
+        public int TutorId { get; set; }
+        public string Place { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public int GroupOwnerId { get; set; }
+    
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<EditGroupVm, StudyBuddy.Domain.Models.Group>()
+                .ReverseMap();
+        }
+    }
+}
